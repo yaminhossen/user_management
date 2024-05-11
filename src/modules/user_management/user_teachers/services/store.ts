6fee9667eec5ba624d45de9183cdc23e2a18e441
Yamin Hossain <streatdot616@gmail.com>
2024-05-11 15:48:08 +0600

    user teachre compltete

---
+++ src/modules/user_management/user_teachers/services/store.ts
@@ -0,0 +1,84 @@
+import db from '../models/db';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import { body, validationResult } from 'express-validator';
+import {
+    anyObject,
+    responseObject,
+    Request,
+} from '../../../common_types/object';
+import response from '../helpers/response';
+import { InferCreationAttributes } from 'sequelize';
+import custom_error from '../helpers/custom_error';
+import error_trace from '../helpers/error_trace';
+
+async function validate(req: Request) {
+    await body('name')
+        .not()
+        .isEmpty()
+        .withMessage('the name field is required')
+        .run(req);
+
+    await body('email')
+        .not()
+        .isEmpty()
+        .withMessage('the preferred name field is required')
+        .run(req);
+    await body('phone_number')
+        .not()
+        .isEmpty()
+        .withMessage('the preferred name field is required')
+        .run(req);
+    await body('image')
+        .not()
+        .isEmpty()
+        .withMessage('the preferred name field is required')
+        .run(req);
+    await body('password')
+        .not()
+        .isEmpty()
+        .withMessage('the preferred name field is required')
+        .run(req);
+
+    let result = await validationResult(req);
+
+    return result;
+}
+
+async function store(
+    fastify_instance: FastifyInstance,
+    req: FastifyRequest,
+): Promise<responseObject> {
+    /** validation */
+    let validate_result = await validate(req as Request);
+    if (!validate_result.isEmpty()) {
+        return response(422, 'validation error', validate_result.array());
+    }
+
+    /** initializations */
+    let models = await db();
+    let body = req.body as anyObject;
+    let data = new models.UserTeachersModel();
+
+    let inputs: InferCreationAttributes<typeof data> = {
+        name: body.name,
+        email: body.email,
+        phone_number: body.phone_number,
+        image: body.image,
+        password: body.password
+    };
+
+    /** print request data into console */
+    // console.clear();
+    // (fastify_instance as any).print(inputs);
+
+    /** store data into database */
+    try {
+        (await data.update(inputs)).save();
+        return response(200, 'data created', data);
+    } catch (error: any) {
+        let uid = await error_trace(models, error, req.url, req.body);
+        throw new custom_error('server error', 500, error.message, uid);
+    }
+}
+
+export default store;
