a0ceb9ec519d5e87e3e0a01add8e7b54c9193db8
Yamin Hossain <streatdot616@gmail.com>
2024-05-18 19:29:08 +0600

    user management customize

---
+++ src/modules/user_management/migrate.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo ""
+echo "user-admin seed start"
+API_URL="http://127.0.0.1:5000/api/v1/admin-users?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_admin/models/seeders
+echo "user-admin seed end"
+echo ""
+
+echo ""
+echo "user-branch-admin seed start"
+API_URL="http://127.0.0.1:5000/api/v1/user-branch-admins?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_branch_admins/models/seeders
+echo "user-branch-admin seed end"
+echo ""
+
+echo ""
+echo "user-parents seed start"
+API_URL="http://127.0.0.1:5000/api/v1/user-parents?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_parents/models/seeders
+echo "user-parents seed end"
+echo ""
+
+echo ""
+echo "user-staffs seed start"
+API_URL="http://127.0.0.1:5000/api/v1/user-staffs?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_staffs/models/seeders
+echo "user-staffs seed end"
+echo ""
+
+echo ""
+echo "user-students seed start"
+API_URL="http://127.0.0.1:5000/api/v1/user-students?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_students/models/seeders
+echo "user-students seed end"
+echo ""
+
+echo ""
+echo "user-teachers seed start"
+API_URL="http://127.0.0.1:5000/api/v1/user-teachers?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+response=$(curl -s "$API_URL")
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_teachers/models/seeders
+echo "user-teachers seed end"
+echo ""
+
+
+
