6ab3359beb6a731785e054d514675cdb799bf696
Yamin Hossain <streatdot616@gmail.com>
2024-07-11 14:57:46 +0600

    asset audits

---
+++ src/modules/asset_management/asset_audits/models/seeders/asset_audits_seeder.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+
+        let data = [];
+        function set_data(id, branch_id, title, date, description) {
+            data.push({
+                id,
+                branch_id,
+                title,
+                date,
+                description,
+                created_at: '2024-02-14',
+                updated_at: '2024-02-14',
+            });
+        }
+
+        set_data(
+            1,
+            1,
+            'Furniture audits',
+            '2024-02-14',
+            'On 14 february furniture audits',
+        );
+        set_data(
+            2,
+            2,
+            'Electric device audits',
+            '2024-02-14',
+            'On 14 february electric device audits',
+        );
+        set_data(
+            3,
+            3,
+            'Building audits',
+            '2024-02-14',
+            'On 14 february building audits',
+        );
+
+        await queryInterface.bulkDelete('asset_audits', null, {});
+        await queryInterface.bulkInsert('asset_audits', data, {});
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         
+         npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/user_management/user_admin/models/seeders
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('asset_audits', null, {});
+    },
+};
--- src/modules/asset_management/migrate.sh
+++ src/modules/asset_management/migrate.sh
@@ -18,12 +18,20 @@
 # echo "asset types seed end"
 # echo ""
 
+# echo ""
+# echo "assets seed start"
+# API_URL="http://127.0.0.1:5000/api/v1/assets?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+# response=$(curl -s "$API_URL")
+# npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/asset_management/assets/models/seeders
+# echo "assets seed end"
+# echo ""
+
 echo ""
-echo "assets seed start"
-API_URL="http://127.0.0.1:5000/api/v1/assets?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+echo "asset audits seed start"
+API_URL="http://127.0.0.1:5000/api/v1/asset-audits?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
 response=$(curl -s "$API_URL")
-npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/asset_management/assets/models/seeders
-echo "assets seed end"
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/asset_management/asset_audits/models/seeders
+echo "asset audits seed end"
 echo ""
 
 
