4404996a28f4d9c0a846461385e9a82efd4fe5d2
Yamin Hossain <streatdot616@gmail.com>
2024-07-11 16:08:02 +0600

    asset audit item done

---
+++ src/modules/asset_management/asset_audit_items/models/asset_audit_items_model.ts
@@ -0,0 +1,97 @@
+import {
+    // Association,
+    DataTypes,
+    // HasManyAddAssociationMixin,
+    // HasManyCountAssociationsMixin,
+    // HasManyCreateAssociationMixin,
+    // HasManyGetAssociationsMixin,
+    // HasManyHasAssociationMixin,
+    // HasManySetAssociationsMixin,
+    // HasManyAddAssociationsMixin,
+    // HasManyHasAssociationsMixin,
+    // HasManyRemoveAssociationMixin,
+    // HasManyRemoveAssociationsMixin,
+    Model,
+    // ModelDefined,
+    // Optional,
+    Sequelize,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+    DefaultSetOptions,
+    // NonAttribute,
+    // ForeignKey,
+} from 'sequelize';
+
+const tableName = 'asset_audit_items';
+const modelName = 'AssetAuditItemsModel';
+
+type Infer = InferAttributes<DataModel>;
+type InferCreation = InferCreationAttributes<DataModel>;
+type feedback = 'available' | 'lost' | 'waste' | 'date over';
+type status = 'active' | 'deactive';
+
+class DataModel extends Model<Infer, InferCreation> {
+    declare id?: CreationOptional<number>;
+
+    declare branch_id: number;
+    declare asset_audit_id: number;
+    declare asset_id: number;
+    declare feedback: feedback;
+
+    declare status?: status;
+    declare creator?: number;
+
+    declare created_at?: CreationOptional<Date>;
+    declare updated_at?: CreationOptional<Date>;
+}
+
+function init(sequelize: Sequelize) {
+    DataModel.init(
+        {
+            id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            branch_id: {
+                type: new DataTypes.BIGINT().UNSIGNED,
+                allowNull: true,
+            },
+            asset_audit_id: {
+                type: new DataTypes.BIGINT().UNSIGNED,
+                allowNull: true,
+            },
+            asset_id: {
+                type: new DataTypes.BIGINT().UNSIGNED,
+                allowNull: true,
+            },
+            feedback: {
+                type: DataTypes.ENUM('available', 'lost', 'waste', 'date over'),
+                allowNull: true,
+            },
+
+            status: {
+                type: new DataTypes.ENUM('active', 'deactive'),
+                defaultValue: 'active',
+            },
+            creator: {
+                type: new DataTypes.TINYINT(),
+                allowNull: true,
+                defaultValue: null,
+            },
+            created_at: DataTypes.DATE,
+            updated_at: DataTypes.DATE,
+        },
+        {
+            tableName: tableName,
+            modelName: modelName,
+            sequelize, // passing the `sequelize` instance is required
+            underscored: true,
+        },
+    );
+
+    return DataModel;
+}
+
+export { init, DataModel };
--- src/modules/asset_management/migrate.sh
+++ src/modules/asset_management/migrate.sh
@@ -26,12 +26,20 @@
 # echo "assets seed end"
 # echo ""
 
+# echo ""
+# echo "asset audits seed start"
+# API_URL="http://127.0.0.1:5000/api/v1/asset-audits?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+# response=$(curl -s "$API_URL")
+# npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/asset_management/asset_audits/models/seeders
+# echo "asset audits seed end"
+# echo ""
+
 echo ""
-echo "asset audits seed start"
-API_URL="http://127.0.0.1:5000/api/v1/asset-audits?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
+echo "asset audit items seed start"
+API_URL="http://127.0.0.1:5000/api/v1/asset-audit-items?orderByCol=id&orderByAsc=true&show_active_data=true&paginate=10&page=0"
 response=$(curl -s "$API_URL")
-npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/asset_management/asset_audits/models/seeders
-echo "asset audits seed end"
+npx sequelize-cli db:seed:all --config src/configs/db.json --seeders-path src/modules/asset_management/asset_audit_items/models/seeders
+echo "asset audit items seed end"
 echo ""
 
 
